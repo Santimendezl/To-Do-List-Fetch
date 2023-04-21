76dabb51f42d7692cfd6cf1b1036f610c158e1b0
Santimendezl <santimlacosta@gmail.com>
2023-04-21 13:00:59 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 	return (
 		<div className="d-flex flex-column justify-content-center align-items-center mt-5" >
 			<h1>To Do List</h1>
-			<div className="vstack gap-2 col-md-5 mx-auto shadow-lg mb-5 bg-body text-center border ps-5 pt-3" style={{width:'75%'}}>
+			<div className="vstack gap-1 col-md-5 mx-auto shadow-lg mb-5 bg-body text-center border ps-5 pt-3" style={{width:'75%'}}>
 			<input type="text" className="border border-0" onChange={e => setInputValue(e.target.value)} value={inputValue} placeholder="Write a task..."/>
 			<hr className="border border-opacity-25"></hr>
 			<p>{inputValue}</p>
