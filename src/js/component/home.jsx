9030eb9dad041db557aaec44ab1c7ba614efe45e
Santimendezl <santimlacosta@gmail.com>
2023-04-21 12:10:09 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue ] = useState('');
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column justify-content-center align-items-center mt-5" >
+			<h1>To Do List</h1>
+			<div className="vstack gap-2 col-md-5 mx-auto shadow-lg mb-5 bg-body text-center border ps-5 pt-3" style={{width:'75%'}}>
+			<input type="text" className="border border-0" onChange={e => setInputValue(e.target.value)} value={inputValue} placeholder="Write a task..."/>
+			<hr className="border border-opacity-25"></hr>
+			<p>{inputValue}</p>
+			</div>
 		</div>
 	);
 };
